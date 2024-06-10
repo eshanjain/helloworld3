a19b24b337e469c7c9c21ae4387eb53a49e9ad80
eshanjain <eshanjain1000@gmail.com>
2024-06-10 10:26:23 -0700

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,25 @@
+# Printing xcode and supported sdks/simulators version installed.
+
+
+
+echo "########################################################################"
+
+
+
+echo "##              PRINTING XCODE AND SDKS/SIMULATORS VERSIONS           ##"
+
+
+
+echo "########################################################################"
+
+
+
+
+
+
+
+echo "Versions of xcode Installed:"
+
+
+
+xcodebuild -version
